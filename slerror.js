c0f2cfa09200ca7fe9ce451d796651ff9f5a8152
slerror <106365747+slerror@users.noreply.github.com>
2022-05-27 10:29:18 +0530

    Create slerror.js

---
+++ slerror.js
@@ -0,0 +1,26 @@
+/**
+* @param trainingExamples_ {Array<Array>} each row of the outer array is an array of 2 elements, 
+*                                         for input and output part of the training example 
+* @param trainingExampleErrorFn {(expectedOutput:any, actualOutput:any)=>any}
+* @param candSlnPredictionFn {(candSln:any,input:any)=>output:any}
+*/
+export function initSlError(trainingExamples_, trainingExampleErrorFn, candSlnPredictionFn) {
+  trainingExamples = trainingExamples_
+  trainingExampleError = trainingExampleErrorFn
+  predict = candSlnPredictionFn
+}
+
+export function slError(candSln) {
+  let totalError = 0
+  for(const trainingExample of trainingExamples) {
+    const input = trainingExample[0]
+    const expectedOutput = trainingExample[1]
+    const actualOutput = predict(candSln, input)
+    totalError += trainingExampleError(expectedOutput, actualOutput)
+  }
+  return totalError
+}
+
+let trainingExamples
+let trainingExampleError
+let predict
