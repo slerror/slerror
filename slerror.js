134abb6eaf0a8054eb1664343d64722ea2a9755c
slerror <106365747+slerror@users.noreply.github.com>
2022-05-27 12:18:19 +0530

    Update slerror.js

--- slerror.js
+++ slerror.js
@@ -2,23 +2,23 @@
 * @param trainingExamples_ {Array<Array>} each element of the outer array is an array of 2 elements. 
 *                  These 2 elements correspond to the input and output part of the training example. 
 * @param trainingExampleErrorFn {(expectedOutput:any, actualOutput:any)=>any}
-* @param candSlnPredictionFn {(candSln:any,input:any)=>output:any}
+* @param mlModelPredictionFn {(mlModel:any,input:any)=>output:any}
 * Acronymns:
 * SL = supervised learning
-* candSln = candidate solution
+* mlModel = machine learning model/system
 */
-export function initSlError(trainingExamples_, trainingExampleErrorFn, candSlnPredictionFn) {
+export function initSlError(trainingExamples_, trainingExampleErrorFn, mlModelPredictionFn) {
   trainingExamples = trainingExamples_
   trainingExampleError = trainingExampleErrorFn
-  predict = candSlnPredictionFn
+  predict = mlModelPredictionFn
 }
 
-export function slError(candSln) {
+export function slError(mlModel) {
   let totalError = 0
   for(const trainingExample of trainingExamples) {
     const input = trainingExample[0]
     const expectedOutput = trainingExample[1]
-    const actualOutput = predict(candSln, input)
+    const actualOutput = predict(mlModel, input)
     totalError += trainingExampleError(expectedOutput, actualOutput)
   }
   return totalError
